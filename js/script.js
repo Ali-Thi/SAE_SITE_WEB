5f35f32f9dc40d3f8c6644ff7123d9b5df58f2de
Ali-Thi <ali.talhaoui95@gmail.com>
2022-10-03 00:06:48 +0200

    ajout d'animations, manque porblème d'alignement du sous menu lors du survol

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-$(document).ready(function () {
-  $(".nav-item").hover(function () {
+$(init);
+
+function init() {
+  new WOW().init();
+
+  $("body").addClass("wow animate__animated animate__fadeIn");
+  $("header").addClass("wow animate__animated animate__fadeIn");
+  $(".nav-link").addClass("wow animate__animated animate__fadeIn");
+  $(".dropdown-menu").addClass("wow animate__animated animate__fadeIn");
+  $("footer").addClass("wow animate__animated animate__fadeIn");
+
+  /*$(".dropdown").hover(function () {
     $(this).children(".dropdown-menu").animate({
-      height: 'toggle'
+      height:"toggle"
     });
-  });
-});
+  });*/
+}
