fa442e80392e6d4b72478479f7a289db08fa7fcd
Ali-Thi <ali.talhaoui95@gmail.com>
2022-10-09 18:37:56 +0200

    ajout animations logo

--- js/script.js
+++ js/script.js
@@ -1,36 +1,47 @@
 $(init);
 
-function init() {
-  new WOW().init();
+var prevScrollpos;
 
+function init() {
+  //ANIMATIONS
+  new WOW().init();
   $("body").addClass("wow animate__animated animate__fadeIn");
   $("header").addClass("wow animate__animated animate__fadeIn");
   $(".nav-link").addClass("wow animate__animated animate__fadeIn");
-  $(".dropdown-menu").addClass("wow animate__animated animate__fadeIn");
+  // $(".dropdown-menu").addClass("wow animate__animated animate__fadeIn");
   $("footer").addClass("wow animate__animated animate__fadeIn");
 
-  let scrollPos = 0;
-  const mainNav = document.getElementById('mainNav');
-  const headerHeight = mainNav.clientHeight;
-  window.addEventListener('scroll', function () {
-    if (this.screen.width >= 992) {
-      const currentTop = document.body.getBoundingClientRect().top * -1;
-      if (currentTop < scrollPos) {
-        // Scrolling Up
-        if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
-          mainNav.classList.add('is-visible');
-        } else {
-          console.log(123);
-          mainNav.classList.remove('is-visible', 'is-fixed');
-        }
-      } else {
-        // Scrolling Down
-        mainNav.classList.remove(['is-visible']);
-        if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
-          mainNav.classList.add('is-fixed');
-        }
-      }
-      scrollPos = currentTop;
-    }
-  });
+  //CHANGEMENT DU LOGO
+  onResize();
+  addEventListener('resize', onResize);
+
+  //ANIMATION NAVBAR LORS DU SCROLL
+  prevScrollpos = window.pageYOffset;
+  addEventListener('scroll', onScroll);
+
+}
+
+function onResize() {
+  if (window.innerWidth >= 1200) {
+    $("#logo").attr("src", 'Images/logo_beauvaisis_blanc_inline.png');
+    $("#logo").css("width", 553);
+  }
+  else if (window.innerWidth >= 890) {
+    $("#logo").attr("src", 'Images/logo_beauvaisis_blanc.png');
+    $("#logo").css("width", 200);
+  }
+  else {
+    $("#logo").attr("src", 'Images/logo_compresser_blanc.png');
+    $("#logo").css("width", 90);
+  }
+}
+
+function onScroll() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    $("header").css("top", 0);
+  } else {
+    $("header").css("top", -130);
+  }
+  prevScrollpos = currentScrollPos;
 }
